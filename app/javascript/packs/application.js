fd2772cdacf02811df861f0a6a2cfca21f573985
CaitQuigs <cquigley13@gmail.com>
2020-08-26 15:29:25 -0400

    Configure webpacker, setup home page and slideout menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("bootstrap");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +17,19 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap";
+import "../stylesheets/application.scss";
+
+global.$ = jQuery;
+
+$(document).ready(function() {
+	$('#menu-button').on('click', function() {
+		$('#menu').animate({ left: 0 }, 'slow');
+		$('#menu-button').hide();
+	});
+	$('#close-button').on('click', function() {
+		$('#menu').animate({ left: -240 }, 'slow');
+		$('#menu-button').show();
+	});
+})
